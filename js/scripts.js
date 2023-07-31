d200ddb8983d0f9d59b28eb63729b1b86a32330b
Nwilso21 <noahwilson6381@gmail.com>
2023-07-31 17:45:35 -0600

    Updated Scripts.js
    Removed all old code, and changed to add pokemon list, as well as pokemon with their attributes. Also added a loop to write out each pokemon in the list for testing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-alert('Hello world');
-var favoriteFood = "tacos";
-document.write(favoriteFood);
+
+let pokemonList = [
+    {Name: 'Cloyster', Height: 1.5, Type: 'Water'},
+    {Name: 'Rhydon', Height: 1.9, Type: 'Ground'},
+    {Name: 'Wooper', Height: .4, Type: 'Water'},
+];
+
+for(i =0; i< pokemonList.length; i++){
+    document.write(pokemonList[i].Name);
+    document.write(pokemonList[i].Height);
+    document.write(pokemonList[i].Type);
+}
