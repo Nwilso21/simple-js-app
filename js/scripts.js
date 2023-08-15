817c544fbbb3af65b7323bed0e4e05ec10d92240
Nwilso21 <noahwilson6381@gmail.com>
2023-08-15 17:34:01 -0600

    Updated Scripts
    Removed the old list setup and loops in order to add forEach as well as a new repository with IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 
-let pokemonList = [
-    {Name: 'Cloyster', Height: 1.5, Type: 'Water'},
-    {Name: 'Rhydon', Height: 1.9, Type: 'Ground'},
-    {Name: 'Wooper', Height: .4, Type: 'Water'},
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {Name: 'Cloyster', Height: 1.5, Type: 'Water'},
+        {Name: 'Rhydon', Height: 1.9, Type: 'Ground'},
+        {Name: 'Wooper', Height: .4, Type: 'Water'},
+    ];
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
 
-for(i =0; i< pokemonList.length; i++){
-    document.write(pokemonList[i].Name + ' (' + pokemonList[i].Height + ')');
-    if(pokemonList[i].Height>1.0){
+  
+pokemonRepository.getAll().forEach(function(pokemon){
+    document.write(pokemon.Name + ' (' + pokemon.Height + ')');
+    if(pokemon.Height>1.0){
         document.write(' - Wow that is big!');
     }
     document.write("\n");
-}
+});
